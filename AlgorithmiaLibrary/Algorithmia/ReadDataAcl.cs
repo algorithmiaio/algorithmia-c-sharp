159a29210be4ff035e48e28d55ad9e914cbedfe2
James Athappilly <jamesatha@gmail.com>
2017-06-28 01:09:55 -0700

    Adding comments to the ReadDataAcl class

--- AlgorithmiaLibrary/Algorithmia/ReadDataAcl.cs
+++ AlgorithmiaLibrary/Algorithmia/ReadDataAcl.cs
@@ -2,29 +2,43 @@
 
 namespace Algorithmia
 {
+    /// <summary>
+    /// Read data ACLs for data directories.
+    /// </summary>
     public class ReadDataAcl
     {
         private const string PUBLIC_PERMISSIONS = "user://*";
         private const string MY_ALGOS_PERMISSIONS = "algo://.my/*";
 
+        /// <summary>
+        /// The private ACL - only your algorithms while you call them can read this directory.
+        /// </summary>
         public static readonly ReadDataAcl PRIVATE = new ReadDataAcl(new List<string>());
+
+        /// <summary>
+        /// The public ACL - any algorithm call can read from this directory.
+        /// </summary>
         public static readonly ReadDataAcl PUBLIC = new ReadDataAcl(new List<string>() { PUBLIC_PERMISSIONS });
+
+        /// <summary>
+        /// My algorithms ACL - any of my algorithms called by anyone on the platform can read this directory.
+        /// </summary>
         public static readonly ReadDataAcl MY_ALGOS = new ReadDataAcl(new List<string>() { MY_ALGOS_PERMISSIONS });
 
 
         private readonly List<string> aclStrings;
 
-        public ReadDataAcl(List<string> acls)
+        internal ReadDataAcl(List<string> acls)
         {
             aclStrings = acls;
         }
 
-        public List<string> getAclStrings()
+        internal List<string> getAclStrings()
         {
             return aclStrings;
         }
 
-        public static ReadDataAcl fromAclStrings(List<string> aclStrings)
+        internal static ReadDataAcl fromAclStrings(List<string> aclStrings)
         {
             if (aclStrings == null)
             {
