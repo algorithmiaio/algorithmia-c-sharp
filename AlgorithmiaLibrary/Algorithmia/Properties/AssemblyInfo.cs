ca2885a964dbf00e39d074d7234b17b8387b2c5b
Ed Blankenship <ed@algorithmia.com>
2017-11-13 10:17:46 -0800

    Automating the build & deploy to NuGet.org.  Going to automate versioning through the build process.

--- AlgorithmiaLibrary/Algorithmia/Properties/AssemblyInfo.cs
+++ AlgorithmiaLibrary/Algorithmia/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-// Information about this assembly is defined by the following attributes. 
+// Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Algorithmia")]
@@ -16,9 +16,9 @@
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyVersion("0.0.0")]
 
-// The following attributes are used to specify the signing key for the assembly, 
+// The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
